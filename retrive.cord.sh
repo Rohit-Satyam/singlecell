552f2231df50e4f95ba076cd88bd79ba8adbc8b8
Rohit Satyam <rohitsatyam102@gmail.com>
2021-05-12 23:41:12 +0530

    Create retrive.cord.sh

---
+++ retrive.cord.sh
@@ -0,0 +1,6 @@
+while read a b; 
+do 
+mod=$(echo $a |tr '-' '_'); 
+coord=$(grep $mod PlasmoDB-37_Pfalciparum3D7.gff | grep -w gene | awk -v var=$mod '{print var,":",$4,$5}' ); 
+echo  $a,$b,$coord; 
+done < kaust_vargene_list.tsv 
