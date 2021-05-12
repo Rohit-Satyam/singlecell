56c33dcee428aedc9b42638de93d3b0d45e692cf
Rohit Satyam <rohitsatyam102@gmail.com>
2021-05-13 02:16:46 +0530

    Update retrive.cord.sh

--- retrive.cord.sh
+++ retrive.cord.sh
@@ -1,6 +1,10 @@
 while read a b; 
 do 
 mod=$(echo $a |tr '-' '_'); 
-coord=$(grep $mod PlasmoDB-37_Pfalciparum3D7.gff | grep -w gene | awk -v var=$mod '{print var":"$4"-"$5}' ); 
-echo  -e $a"\t"$coord"\t"$b >> var_genes_with_coord.csv; 
-done < kaust_vargene_list.tsv
+coord=$(grep $mod PlasmoDB-37_Pfalciparum3D7.gff | grep -w gene | awk {print "pf3d7_"$1":"$4"-"$5}' ); 
+echo  -e $a"\t"$coord"\t"$b >> var_genes_with_coord.csv; done < kaust_vargene_list.tsv
+
+## After this edit in excel the PF3D7 ----> Pf3D7 and prepare a bed file.
+
+## samtools -hb -L var_genes.bed possorted.bam > var_gene_16D.bam
+
