4dc77103fac6be680f0b85b1ce5e342f463971f5
Rohit Satyam <rohitsatyam102@gmail.com>
2021-05-14 19:23:09 +0530

    Update retrive.cord.sh

--- retrive.cord.sh
+++ retrive.cord.sh
@@ -8,3 +8,19 @@ echo  -e $a"\t"$coord"\t"$b >> var_genes_with_coord.csv; done < kaust_vargene_li
 
 ## samtools -hb -L var_genes.bed possorted.bam > var_gene_16D.bam
 
+## Subsetting the bamswith theuniquely mapping reads obtained from STAR.
+
+samtools view -H var_gene_16D.bam> 16D.header
+samtools view var_gene_16D.bam | grep -w -f <(sed 's/.*/\\<&\\>/' var.genes) | grep -w -f 16D_unique_readID.txt > 16Dunique.sam ## because our bam containes read IDs for rifins, surfins etc too.
+cat 16D.header 16Dunique.sam | samtools view -bh - > 16D_new.bam
+
+python processing_for_genes.py -b 16D_new.bam -i 16D_new.bam.bai -c 16D_count.csv -p 16D_results.pdf
+
+
+### References
+## https://github.com/alexdobin/STAR/issues/460
+## https://www.biostars.org/p/243020/#:~:text=%22too%20short%22%20means%20that%20the,of%20mates)%20should%20be%20mapped
+## https://www.ecseq.com/support/ngs-snippets/how-to-extract-a-list-of-specific-read-IDs-from-a-BAM-file
+
+
+
